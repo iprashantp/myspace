377f0e055b889a9bba42b46f0b73a94fcaf35342
patep2 <prashant.patel@in.pega.com>
2020-07-22 20:39:16 +0530

    added first spring project

---
+++ SpringFire/src/main/java/com/ravens/Alien.java
@@ -0,0 +1,21 @@
+package com.ravens;
+
+public class Alien {
+
+	private int age = 0;
+
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Alien() {
+		// TODO Auto-generated constructor stub
+		System.out.println("created object of Alien");
+	}
+	public void printer() {
+		System.out.println("printer of Alien");
+	}
+
+}
---
+++ SpringFire/src/main/java/com/ravens/App.java
@@ -0,0 +1,24 @@
+package com.ravens;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+    	//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	ApplicationContext factoryx = new ClassPathXmlApplicationContext("spring.xml");
+        Alien obj = (Alien)factoryx.getBean("alien");
+        obj.printer();
+        //obj.setAge(20);
+        System.out.println("obj1 age: "+obj.getAge());
+        
+        Alien obj2 = (Alien) factoryx.getBean("alien");
+        System.out.println("obj2 age: "+obj2.getAge());
+        
+    }
+}
